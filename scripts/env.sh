9ae1a44f1234a516b62f25f3d70df311b08bd63f
Rodrigo Rosa <rodrigo.sr6@gmail.com>
2023-12-09 17:48:13 -0300

    ci: teste 11

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,7 +7,7 @@ then
     POSTGRES_DB="wbr_db"
     POSTGRES_USER="wbr_user"
     POSTGRES_PASSWORD=${DB_PASSWORD}
-    DATABASE_HOST="dpg-clq50eqe9h4c73aib1s0-a"
+    DATABASE_HOST="dpg-clq50eqe9h4c73aib1s0-a.ohio-postgres.render.com"
     DATABASE_PORT=5432
 else
     POSTGRES_DB="wbr_db"
